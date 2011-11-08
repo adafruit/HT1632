56176edaba3ad141d487a895a879f0b88bd53882
Ladyada <limor@ladyada.net>
2011-11-08 11:19:17 -0500

    initial commit

---
+++ HT1632.cpp
@@ -0,0 +1,129 @@
+#include <WProgram.h>
+#include "HT1632.h"
+
+ HT1632::HT1632(int8_t data, int8_t wr, int8_t cs, int8_t rd) {
+  _data = data;
+  _wr = wr;
+  _cs = cs;
+  _rd = rd;
+
+  for (uint8_t i=0; i<48; i++) {
+    ledmatrix[i] = 0;
+  }
+}
+
+void HT1632::begin(uint8_t type) {
+  pinMode(_cs, OUTPUT);
+  digitalWrite(_cs, HIGH);
+  pinMode(_wr, OUTPUT);
+  digitalWrite(_wr, HIGH);
+  pinMode(_data, OUTPUT);
+  
+  if (_rd >= 0) {
+    pinMode(_rd, OUTPUT);
+    digitalWrite(_rd, HIGH);
+  }
+
+  sendcommand(HT1632_SYS_EN);
+  sendcommand(HT1632_LED_ON);
+  sendcommand(HT1632_BLINK_OFF);
+  sendcommand(HT1632_MASTER_MODE);
+  sendcommand(HT1632_INT_RC);
+  sendcommand(type);
+  sendcommand(HT1632_PWM_CONTROL | 0xF);
+  
+  WIDTH = 24;
+  HEIGHT = 16;
+}
+
+
+void HT1632::clrPixel(uint8_t x, uint8_t y) {
+  ledmatrix[(y*WIDTH+x) / 8] &= ~_BV(7 - (y*WIDTH+x) % 8);
+}
+
+void HT1632::setPixel(uint8_t x, uint8_t y) {
+  ledmatrix[(y*WIDTH+x) / 8] |= _BV(7 - (y*WIDTH+x) % 8);
+}
+
+void HT1632::dumpScreen() {
+Serial.println("---------------------------------------");
+  for (uint16_t i=0; i<(WIDTH*HEIGHT/8); i++) {
+    Serial.print("0x");
+    Serial.print(ledmatrix[i], HEX);
+    Serial.print(" ");
+    if (i % 3 == 2) Serial.println();
+  }
+
+  Serial.println("\n---------------------------------------");
+}
+
+void HT1632::writeScreen() {
+  for (uint16_t i=0; i<(WIDTH*HEIGHT/8); i++) {
+    writeRAM(2*i, ledmatrix[i] >> 4);
+    writeRAM(2*i+1, ledmatrix[i] & 0xF);
+  }
+}
+
+
+void HT1632::clearScreen() {
+  for (uint8_t i=0; i<(WIDTH*HEIGHT/8); i++) {
+    ledmatrix[i] = 0;
+  }
+  writeScreen();
+}
+
+
+void HT1632::writedata(uint16_t d, uint8_t bits) {
+  //Serial.println(d, HEX);
+  pinMode(_data, OUTPUT);
+  for (uint8_t i=bits; i > 0; i--) {
+    digitalWrite(_wr, LOW);
+   //Serial.println(_BV(i-1), HEX);
+   if (d & _BV(i-1)) {
+     digitalWrite(_data, HIGH);
+   } else {
+     digitalWrite(_data, LOW);
+   }
+  digitalWrite(_wr, HIGH);
+  }
+  pinMode(_data, INPUT);
+}
+
+
+
+
+void HT1632::writeRAM(uint8_t addr, uint8_t data) {
+  //Serial.print("Writing 0x"); Serial.print(data&0xF, HEX);
+  //Serial.print(" to 0x"); Serial.println(addr & 0x7F, HEX);
+
+  uint16_t d = HT1632_WRITE;
+  d <<= 7;
+  d |= addr & 0x7F;
+  d <<= 4;
+  d |= data & 0xF;
+ 
+  digitalWrite(_cs, LOW);
+  writedata(d, 14);
+  digitalWrite(_cs, HIGH);
+}
+
+
+uint8_t HT1632::sendcommand(uint8_t cmd) {
+  uint16_t data = 0;
+  data = HT1632_COMMAND;
+  data <<= 8;
+  data |= cmd;
+  data <<= 1;
+  
+  digitalWrite(_cs, LOW);
+  writedata(data, 12);
+  digitalWrite(_cs, HIGH);  
+}
+
+
+void HT1632::fillScreen() {
+  for (uint8_t i=0; i<(WIDTH*HEIGHT/8); i++) {
+    ledmatrix[i] = 0xFF;
+  }
+  writeScreen();
+}
---
+++ HT1632.h
@@ -0,0 +1,53 @@
+#include <WProgram.h>
+
+#define HT1632_READ  0x6
+#define HT1632_WRITE 0x5
+#define HT1632_COMMAND 0x4
+
+#define HT1632_SYS_DIS 0x00
+#define HT1632_SYS_EN 0x01
+#define HT1632_LED_OFF 0x02
+#define HT1632_LED_ON 0x03
+#define HT1632_BLINK_OFF 0x08
+#define HT1632_BLINK_ON 0x09
+#define HT1632_SLAVE_MODE 0x10
+#define HT1632_MASTER_MODE 0x14
+#define HT1632_INT_RC 0x18
+#define HT1632_EXT_CLK 0x1C
+#define HT1632_PWM_CONTROL 0xA0
+
+#define HT1632_COMMON_8NMOS  0x20
+#define HT1632_COMMON_16NMOS  0x24
+#define HT1632_COMMON_8PMOS  0x28
+#define HT1632_COMMON_16PMOS  0x2C
+
+class HT1632 {
+
+ public:
+  HT1632(int8_t data, int8_t wr, int8_t cs, int8_t rd = -1);
+
+  void begin(uint8_t type);
+  
+
+  void clrPixel(uint8_t x, uint8_t y);
+  void setPixel(uint8_t x, uint8_t y);
+
+  void clearScreen();
+  void fillScreen();
+  void writeScreen();
+  void dumpScreen();
+
+  /*
+  void blink(void);
+  void blinkOff(void);
+
+  void setBrightness(uint8_t pwm);
+  */
+ private:
+  int8_t WIDTH, HEIGHT;
+  int8_t _data, _cs, _wr, _rd;
+  uint8_t ledmatrix[48];     // 16 * 24 / 8
+  uint8_t sendcommand(uint8_t c);
+  void writedata(uint16_t d, uint8_t bits);
+  void writeRAM(uint8_t addr, uint8_t data);
+};
